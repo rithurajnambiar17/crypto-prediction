9b5b60c31712c954ac6be6e050fdb2406051e824
Rithuraj Nambiar <69073735+rithurajnambiar17@users.noreply.github.com>
2022-08-24 22:23:08 +0530

    structure cleaned!

--- index.py
+++ index.py
@@ -16,57 +16,54 @@ def index():
 @app.route('/result', methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
-        if request.form.get('result') == "plot":
+        coin = str(request.form.get('coin'))
+        if coin == 'None':
+            return render_template('index.html')
 
-            coin = str(request.form.get('coin'))
+        else:
+            if request.form.get('result') == "plot":
+                    today = datetime.date.today()
+                    d1 = today.strftime("%Y-%m-%d")
+                    d2 = datetime.date.today() - datetime.timedelta(days = 9)
+                    d2 = d2.strftime("%Y-%m-%d")
+                    data = yf.download(coin, start = d2, end = d1, progress = False)
+                    candle_plot(data)
+                    return render_template('index.html', coin = coin)
 
-            today = datetime.date.today()
-            d1 = today.strftime("%Y-%m-%d")
-            d2 = datetime.date.today() - datetime.timedelta(days = 9)
-            d2 = d2.strftime("%Y-%m-%d")
-            data = yf.download(coin, start = d2, end = d1, progress = False)
+            elif request.form.get('result') == 'predict':
+                coin = str(request.form.get('coin'))
+                coinDict = {
+                    'BTC-USD' : 'bitcoin',
+                    'DOGE-USD' : 'dogecoin',
+                    'ETH-USD' : 'ethereum',
+                    'MIOTA-USD' : 'iota',
+                    'TRX-USD' : 'tron'
+                }
+                MODEL_PATH = f'./models/{coinDict[coin]}.h5'
+                model = load_model(MODEL_PATH)
 
-            candle_plot(data)
+                today = datetime.date.today()
+                d1 = today.strftime("%Y-%m-%d")
+                d2 = datetime.date.today() - datetime.timedelta(days = 9)
+                d2 = d2.strftime("%Y-%m-%d")
 
-            return render_template('index.html', coin = coin)
-        elif request.form.get('result') == 'predict':
+                new_sample = yf.download(coin, start = d2, end = d1, progress = False)
+                last9 = new_sample[['Close']]
+                vals = last9.values
+                temp = []
 
-            coin = str(request.form.get('coin'))
-            coinDict = {
-                'BTC-USD' : 'bitcoin',
-                'DOGE-USD' : 'dogecoin',
-                'ETH-USD' : 'ethereum',
-                'MIOTA-USD' : 'iota',
-                'TRX-USD' : 'tron'
-            }
-            MODEL_PATH = f'./models/{coinDict[coin]}.h5'
-            model = load_model(MODEL_PATH)
+                for i in range(len(vals)):
+                    temp.append(vals[0][0])
 
-            today = datetime.date.today()
-            d1 = today.strftime("%Y-%m-%d")
-            d2 = datetime.date.today() - datetime.timedelta(days = 9)
-            d2 = d2.strftime("%Y-%m-%d")
+                scaler = MinMaxScaler()
 
-            new_sample = yf.download(coin, start = d2, end = d1, progress = False)
+                sample = scaler.fit_transform(np.array(temp).reshape(-1, 1))
+                sample = np.asarray(sample)[1:]
+                sample = np.reshape(sample, (1, 1, 9))
 
-            last9 = new_sample[['Close']]
-            vals = last9.values
+                prediction = model.predict(sample, batch_size = 2)
+                prediction = scaler.inverse_transform(prediction)
 
-            temp = []
-
-            for i in range(len(vals)):
-                temp.append(vals[0][0])
-
-            scaler = MinMaxScaler()
-            sample = scaler.fit_transform(np.array(temp).reshape(-1, 1))
-            sample = np.asarray(sample)[1:]
-
-            sample = np.reshape(sample, (1, 1, 9))
-
-            prediction = model.predict(sample, batch_size = 2)
-
-            prediction = scaler.inverse_transform(prediction)
-            return render_template('predict.html', prediction=prediction)
+                return render_template('predict.html', prediction=prediction)
 
 app.run(debug=True)
-
